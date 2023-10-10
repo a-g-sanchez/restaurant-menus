5db37af5bcef2f05092a9790dda927b2934af388
a-g-sanchez <aarongsanchez@google.com>
2023-10-10 12:03:01 -0500

    Imported Item into the test file and commented out the duplicate code

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const {sequelize} = require('./db')
-const {Restaurant, Menu} = require('./models/index')
+const {Restaurant, Menu, Item} = require('./models/index')
 const {
     seedRestaurant,
     seedMenu,
@@ -69,39 +69,39 @@ describe('Restaurant and Menu Models', () => {
     
 // });
 
-// describe('test associations', () => {
-//     /**
-//      * Runs the code prior to all tests
-//      */
-//         beforeAll(async () => {
-//             // the 'sync' method will create tables based on the model class
-//             // by setting 'force:true' the tables are recreated each time the 
-//             // test suite is run
-//             await sequelize.sync({ force: true });
-//             await Restaurant.bulkCreate(seedRestaurant);
-//             await Menu.bulkCreate(seedMenu);
-//             await Item.bulkCreate(seedItem);
-//         });
+describe('test associations', () => {
+    /**
+     * Runs the code prior to all tests
+     */
+        beforeAll(async () => {
+            // the 'sync' method will create tables based on the model class
+            // by setting 'force:true' the tables are recreated each time the 
+            // test suite is run
+            await sequelize.sync({ force: true });
+            await Restaurant.bulkCreate(seedRestaurant);
+            await Menu.bulkCreate(seedMenu);
+            await Item.bulkCreate(seedItem);
+        });
     
 
-//     test('a Restaurant has multiple Menus', async () => {
-//         const rest = await Restaurant.findByPk(1);
-//         await rest.addMenu([1, 2]);
-//         const menus = await rest.getMenus();
-//         expect(menus.length).toBe(2);
-//     })
+    test('a Restaurant has multiple Menus', async () => {
+        const rest = await Restaurant.findByPk(1);
+        await rest.addMenu([1, 2]);
+        const menus = await rest.getMenus();
+        expect(menus.length).toBe(2);
+    })
 
-//     test('a Menu has multiple items', async () => {
-//         const menu = await Menu.findByPk(1);
-//         await menu.addItems([1, 2]);
-//         const items = await menu.getItems();
-//         expect(items.length).toBe(2);
-//     })
+    test('a Menu has multiple items', async () => {
+        const menu = await Menu.findByPk(1);
+        await menu.addItems([1, 2]);
+        const items = await menu.getItems();
+        expect(items.length).toBe(2);
+    })
 
-//     test('eager loading', async () => {
-//         const menu1 = await Menu.findByPk(1, {
-//             include: "Items"
-//         });
-//         expect(menu1.Items.length).toBe(2);
-//     })
-// })
+    test('eager loading', async () => {
+        const menu1 = await Menu.findByPk(1, {
+            include: "Items"
+        });
+        expect(menu1.Items.length).toBe(2);
+    })
+})
